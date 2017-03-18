24f9f2d071df40ad7ad52f2ec13d5416d5bb4ab6
Nikki Riser <nriser@Nikkis-MacBook-Pro.local>
2017-03-18 19:20:25 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,20 @@
-var cardOne = "queen"; 
-var cardTwo = "king";
-var cardThree = "queen"; 
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = []; // hold the cards that are in play
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
