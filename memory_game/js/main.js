9d851feaf4096a61dfc67eac37b841aea9d89172
Nikki Riser <nriser@Nikkis-MacBook-Pro.local>
2017-03-22 17:34:19 -0400

    added reset button and score

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,9 +23,20 @@ var cards = [
 
 var cardsInPlay = []; // hold the cards that are in play
 
+// Display score
+var score = document.createElement("p");
+document.getElementById("score").appendChild(score);
+score.innerHTML = "Score: 0";
+var addScore = 0;
+
+
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
+		// add score
+		addScore ++;
+		// display accumulated score
+		score.innerHTML = "Score: " + addScore;
 	} else {
 		alert("Sorry, try again.");
 	}
@@ -50,9 +61,29 @@ var createBoard = function() {
 		var cardElement = document.createElement("img");
 		cardElement.setAttribute("src", "images/back.png");
 		cardElement.setAttribute("data-id", i);
-		cardElement.addEventListener("click", flipCard);
+		cardElement.addEventListener("click", flipCard); //
 		document.getElementById("game-board").appendChild(cardElement);
 	}
 };
 
 createBoard();
+
+var reset = function() {
+	var images = document.getElementsByTagName("img");
+	
+	for (var i = 0; i < images.length; i++) {
+		images[i].removeAttribute("src");
+		images[i].setAttribute("src", "images/back.png");
+	}
+	// empty out cardsInPlay array
+	for (var i = 0; i = cardsInPlay.length; i++) {
+		cardsInPlay.pop();
+	}
+};
+
+var clickButton = function() {
+	var button = document.getElementsByTagName("button")[0];
+	button.addEventListener("click", reset);
+};
+
+clickButton();
